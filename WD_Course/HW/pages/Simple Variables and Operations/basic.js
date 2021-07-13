2b62dcc6b293048ac6b651a8c447a5fd0798f4de
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2021-07-13 16:22:04 +0300

    Add files via upload

---
+++ WD_Course/HW/pages/Simple Variables and Operations/basic.js
@@ -0,0 +1,23 @@
+
+// Задача 1: 
+// Напишете 2 примера за операцията "Делене по модул".
+
+// Task1: Even number 
+let x = 8 ; 
+let even = x % 2 ; 
+console.log(`${x} % 2 == ${even} => The value of X variable is even number.`) ; 
+
+// Task2: Odd number 
+let y = 9 ; 
+let odd = y % 2 ; 
+console.log(`${y} % 2 == ${odd} => The value of Y variable is odd number.`) ; 
+
+// Other example 
+let a = 10 ; 
+let b = 5 ; 
+let result = a % b ; 
+
+console.log(result) ; 
+
+result = ++a % b ; 
+console.log(result) ; 
